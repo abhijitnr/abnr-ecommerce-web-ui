5b2450283cf8bf93d1eee854a2fd617f68daf2ac
abhijitnr <abhijitbiswas8859@gmail.com>
2022-11-13 09:00:14 +0530

    Register and Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
     </>
   );
 }
