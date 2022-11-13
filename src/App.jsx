db06d55d32f2a06548edf33d26ed43c5d1206c0f
abhijitnr <abhijitbiswas8859@gmail.com>
2022-11-13 08:28:21 +0530

    Product page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      {/* <ProductList /> */}
+      <Product />
     </>
   );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,174 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
 function Product() {
-  return <div>Product</div>;
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+
+      <Wrapper>
+        <ImageContainer>
+          <Image
+            src="https://assets.ajio.com/medias/sys_master/root/h0f/h77/14371285434398/-473Wx593H-440984123-blue-MODEL.jpg"
+            alt="image"
+          />
+        </ImageContainer>
+
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Description>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+            rem corporis nostrum, atque similique quos tenetur architecto
+            doloribus error repellat consectetur a voluptas quidem, adipisci
+            aliquam eligendi? Repudiandae, ullam impedit.
+          </Description>
+          <Price>$ 20.00</Price>
+
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+
+          <AddContainer>
+            <AmountContainer>
+              <RemoveOutlinedIcon />
+              <Amount>1</Amount>
+              <AddOutlinedIcon />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
 }
 
 export default Product;
