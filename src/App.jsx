bc650b1b4e24d4fdc212ccb0986f2b4a576c1cef
abhijitnr <abhijitbiswas8859@gmail.com>
2022-11-18 01:02:38 +0530

    Cart page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Cart from "./pages/Cart";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+// import Register from "./pages/Register";
 
 function App() {
   return (
